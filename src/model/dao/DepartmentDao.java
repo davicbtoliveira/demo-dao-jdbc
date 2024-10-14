6c4d3854b184d9f54d341aafd1b1c55b559fc40a
ozzy <davicbtoliveira@hotmail.com>
2024-10-13 21:01:19 -0400

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Department id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
